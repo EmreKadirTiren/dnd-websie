cc4df906884bf00b3b55d2235c1fbafcaa967ca3
Emre Kadir Tiren <77925614+EmreKadirTiren@users.noreply.github.com>
2024-03-15 17:57:22 +0100

    Quartz sync: Mar 15, 2024, 5:57 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Emre Kadir's D&D Notes",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "kluizenaren.eu.org",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
